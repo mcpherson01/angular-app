d0295bdc4bafa6ade641b8375bbf3ee90ce58ae1
Emmett Hoolahan <emmetthoolahan@gmail.com>
2017-11-09 22:12:46 +0800

    lol

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,19 +10,3 @@ export class AppComponent {
 }
 
 
-@Component(...)
-export class MyComponent implements OnInit {
- 
-  results: string[];
- 
-  // Inject HttpClient into your component or service.
-  constructor(private http: HttpClient) {}
- 
-  ngOnInit(): void {
-    // Make the HTTP request:
-    this.http.get('/api/items').subscribe(data => {
-      // Read the result field from the JSON response.
-      this.results = data['results'];
-    });
-  }
-}
